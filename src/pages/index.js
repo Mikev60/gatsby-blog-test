bd4f5b5acd294c187425f2d186a6fbc9b597e0c9
Mikev60 <mickael.vermand@gmail.com>
2021-01-15 09:45:49 +0100

    test of a simple blog made in gatsby js following udemy course

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,63 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const BlogLink = styled(Link)`
+text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+margin-bottom: 20px;
+color: Blue;
+`
+
+const IndexPage = ({ data }) => {
+  console.log(data);
+
+  return <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>Mike's thoughts</h1>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {data.allMarkdownRemark.edges.map(( {node} ) => {
+      return (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+          <BlogTitle>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      )
+    })}
   </Layout>
-)
+}
 
 export default IndexPage
+
+export const query = graphql`
+query MyQuery {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
+    edges {
+      node {
+        frontmatter {
+          date
+          description
+          title
+        }
+        fields {
+          slug
+        }
+        html
+        excerpt
+      }
+    }
+  }
+}
+
+`
